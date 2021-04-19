b0419fcdf50e2fbc4754494944a0de95aa8f8a9e
Giuseppe <carrellagiuseppe@gmail.com>
2021-04-19 23:21:40 +0200

    Inizializzazione Gioco del Secolo

---
+++ Scripts/menu-voice.gd
@@ -0,0 +1,23 @@
+extends Button
+
+func _ready():
+#	Button.set_text('Nuovo Gioco');
+	set_process(false);
+#	VOICES = get_children();
+#	VOICES[0].set("custom_colors/font_color", Color(235, 255, 0, 255));
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass;
+
+func _on_mouse_entered():
+	print('entered');
+	set("custom_colors/font_color", Color(235, 255, 0, 255));
+
+func _on_mouse_exited():
+	print('exited');
+	set("custom_colors/font_color", Color(255, 255, 255, 255));
+	
+func _on_button_up():
+	print('pressed');
+
